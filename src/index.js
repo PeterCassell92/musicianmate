bcdb653ddf732226731410502e50433f84174ca8
Pete <pc0122@my.bristol.ac.uk>
2025-05-05 22:36:21 +0100

    FE-1006 converted to index.tsx and began to convert types based on the backend mongoose schema

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/main.scss'
-import App from './App'
-import dotenv from 'dotenv'
-dotenv.config()
-
-ReactDOM.render(<App />, document.getElementById('root'))

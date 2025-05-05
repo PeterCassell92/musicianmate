5ee8a8a2f580509d512571338be142ea9d1d2375
Pete <pc0122@my.bristol.ac.uk>
2025-05-05 20:38:47 +0100

    added basic lyric editor. no save yet

--- src/components/song/LyricSheet.js
+++ src/components/song/LyricSheet.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Loader from 'react-loader-spinner'
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom'
 import { getSongLyricSheet } from '../../lib/api'
+import LyricEditor from './LyricEditor'
 
 function LyricSheet() {
 
   const history = useHistory()
   const { songId, lyricSheetId } = useParams()
-  const [lyricsText , setLyricsText] = React.useState(null)
+  const [lyricsText , setLyricsText] = useState(null)
+  const [ lyricEditorOpen, setLyricEditorOpen ] = useState(false)
+
+  const onLyricEditorClose = () => setLyricEditorOpen(false)
+
 
   React.useEffect(() => {
     const getData = async () => {
@@ -23,16 +29,29 @@ function LyricSheet() {
   return (
     <div className='container'>
       <h1>lyricSheet</h1>
-
-      <div className='section lyricsBox'>
-        { lyricsText }
-        {/* TODO: Render lyrics with annotation options */}
-      </div>
-      <button className="button" onClick={handleClear}>
-        Edit Lyrics
-      </button>
+      { lyricsText ?
+        lyricEditorOpen ?
+          <>
+            <div className='section lyricsBox'>
+              { lyricsText }
+              {/* TODO: Render lyrics with annotation options */}
+            </div>
+            <button className="button" onClick={()=> setLyricEditorOpen(true)}>
+              Edit Lyrics
+            </button>
+          </>
+          : <LyricEditor onClose={onLyricEditorClose} lyrics={lyricsText} songId={songId}/> :
+        <div id="loader">
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={150}
+            width={150}
+            timeout={3000} //3 secs
+          />
+        </div>
+      }
     </div>
-    
   )
 }
 
